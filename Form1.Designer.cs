fb21545b2b25c2669f679c5939a862ce922abec1
MatthewGimbut <mattgimbut@gmail.com>
2016-10-29 19:39:10 -0400

    Added email capability

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,6 +39,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.email = new System.Windows.Forms.Button();
+            this.to = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -143,11 +148,66 @@
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(256, 234);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(75, 23);
+            this.email.TabIndex = 10;
+            this.email.Text = "Send Email";
+            this.email.UseVisualStyleBackColor = true;
+            this.email.Click += new System.EventHandler(this.email_Click);
+            // 
+            // to
+            // 
+            this.to.BackColor = System.Drawing.Color.DimGray;
+            this.to.ForeColor = System.Drawing.Color.White;
+            this.to.Location = new System.Drawing.Point(387, 433);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(189, 20);
+            this.to.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(335, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "To:";
+            // 
+            // subject
+            // 
+            this.subject.BackColor = System.Drawing.Color.DimGray;
+            this.subject.ForeColor = System.Drawing.Color.White;
+            this.subject.Location = new System.Drawing.Point(387, 459);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(189, 20);
+            this.subject.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(335, 462);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Subject:";
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 500);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.email);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.ReplaceOne);
@@ -178,6 +238,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button email;
+        private System.Windows.Forms.TextBox to;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.Label label6;
     }
 }
 
